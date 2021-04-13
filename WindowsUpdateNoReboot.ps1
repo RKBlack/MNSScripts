47ce0d6e8430bfde4759c35e399fdb8e439a8164
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2021-04-13 16:51:16 -0500

    Update WindowsUpdateNoReboot.ps1

--- WindowsUpdateNoReboot.ps1
+++ WindowsUpdateNoReboot.ps1
@@ -1,4 +1,6 @@
+$KBBlacklist = "KB5000802, KB5000808"
+$PSUpdateLog = "C:\Windows\PSWindowsUpdate.log"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet -Confirm:$false -Force
 Install-Module -Name PSWindowsUpdate -Confirm:$false -Force
-Install-WindowsUpdate -IgnoreReboot -AcceptAll -NotKBArticleID KB5000802, KB5000808
+Install-WindowsUpdate -IgnoreReboot -AcceptAll -NotKBArticleID $KBBlacklist | Out-File $PSUpdateLog -Confirm:$false -Verbose -SkipModuleTest –RunNow
