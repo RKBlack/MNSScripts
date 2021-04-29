c8d7d269335612cdb6db9634afccda0e934df597
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2021-04-29 17:00:51 -0500

    Update Windows10Upgrade20H2AutoReboot.ps1

--- Windows10Upgrade20H2AutoReboot.ps1
+++ Windows10Upgrade20H2AutoReboot.ps1
@@ -8,9 +8,8 @@ $MountResult = Mount-DiskImage -ImagePath $ISO -PassThru
 $driveLetter = (Get-DiskImage -ImagePath $ISO | Get-Volume).DriveLetter
 $setupFile = $driveLetter + ':\setup.exe'
 $finalCommand = $driveLetter + ':\setup.exe /quiet /auto upgrade /Finalize /copylogs C:\Temp\Logfiles /showoobe none'
-Start-Process -FilePath $setupFile -ArgumentList '/quiet /auto upgrade /SkipFinalize /copylogs C:\Temp\Logfiles /showoobe none' | Wait-Process -Name setup -Timeout 7200 -ErrorAction SilentlyContinue
+Start-Process -FilePath $setupFile -ArgumentList '/quiet /auto upgrade /SkipFinalize /copylogs C:\Temp\Logfiles /showoobe none' | Wait-Process -Name setup.exe -Timeout 7200 -ErrorAction SilentlyContinue
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-Module -Name NuGet -Force -Confirm:$false
 Install-PackageProvider -Name NuGet -Force -Confirm:$false
 Install-Module -Name BurntToast -Force -Confirm:$false
 Install-Module -Name RunAsUser -Force -Confirm:$false
