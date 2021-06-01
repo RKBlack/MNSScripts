009d2d98089e8b2368764d95a90cb640014b6e1b
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2021-06-01 09:41:42 -0500

    Update remove-quickassist.ps1

--- remove-quickassist.ps1
+++ remove-quickassist.ps1
@@ -1,4 +1,4 @@
-$apps = Get-WindowsCapability -Online -Name “*QuickAssist*”
+$apps = Get-WindowsCapability -Online -Name "*QuickAssist*"
 foreach($app in $apps)
 {
     Remove-WindowsCapability -Online -Name $app.Name
