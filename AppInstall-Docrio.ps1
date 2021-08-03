3c42c3382bbccb62d66a42588002874ad37db6a9
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2021-08-03 12:12:56 -0500

    Update AppInstall-Docrio.ps1

--- AppInstall-Docrio.ps1
+++ AppInstall-Docrio.ps1
@@ -2,22 +2,8 @@
 $workdir = "c:\Temp\"
 # Check if work directory exists if not create it
 If (Test-Path -Path $workdir -PathType Container)
-{ Write-Host "$workdir already exists" -ForegroundColor Red}
 ELSE
 { New-Item -Path $workdir  -ItemType directory }
-
-#Sleep Timer
-function Start-Sleep($seconds) {
-    $doneDT = (Get-Date).AddSeconds($seconds)
-    while($doneDT -gt (Get-Date)) {
-        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
-        $percent = ($seconds - $secondsLeft) / $seconds * 100
-        Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining $secondsLeft -PercentComplete $percent
-        [System.Threading.Thread]::Sleep(500)
-    }
-    Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining 0 -Completed
-}
-
 # Download the installer
 $source = "https://one-click-edit-application-build-bucket.s3.amazonaws.com/Docrio%20Edit%20Setup%202.6.8.exe"
 $destination = "$workdir\Docrio Edit Setup 2.6.8.exe"
