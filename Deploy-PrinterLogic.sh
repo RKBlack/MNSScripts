2f7c2eb0e1c4735ba942d915ac91e6f673695b12
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2022-01-14 12:02:38 -0600

    Create Deploy-PrinterLogic.sh

---
+++ Deploy-PrinterLogic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo -i
+if ! [ -f /tmp/PrinterInstallerClientSetup.pkg ]; then
+curl -o /tmp/PrinterInstallerClientSetup.pkg "https://$1/client/setup/PrinterInstallerClientSetup.pkg"
+sleep 10
+fi
+installer -allowUntrusted -pkg /tmp/PrinterInstallerClientSetup.pkg -target /
+bash /opt/PrinterInstallerClient/bin/set_home_url.sh https $1
+bash /opt/PrinterInstallerClient/bin/use_authorization_code.sh $2
