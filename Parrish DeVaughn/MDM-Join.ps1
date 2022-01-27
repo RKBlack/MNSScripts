39f115bf286adccf402d9b90a3a36d7fc80f7f0f
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2022-01-27 11:26:33 -0600

    Update MDM-Join.ps1

--- Parrish DeVaughn/MDM-Join.ps1
+++ Parrish DeVaughn/MDM-Join.ps1
@@ -1,3 +1,3 @@
 New-Item -ItemType Directory -Path C:\Temp -ErrorAction SilentlyContinue
-Invoke-WebRequest -Uri https://github.com/RKBlack/MNSScripts/blob/cda79095f13947dd9c17be63a2177a6652a86b49/MDMJOIN.ppkg -OutFile C:\Temp\MDMJOIN.ppkg
+Invoke-WebRequest -Uri https://github.com/RKBlack/MNSScripts/blob/de89bab97e7fa0de7988536bcef74a4b1b8b9d07/Parrish%20DeVaughn/MDMJOIN.ppkg -OutFile C:\Temp\MDMJOIN.ppkg
 Install-ProvisioningPackage -PackagePath C:\Temp\MDMJOIN.pkg -QuietInstall -ForceInstall
