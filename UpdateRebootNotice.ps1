eaa162dcfc2ec5a0f897f905b8898f7440d5ca19
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2022-02-10 17:31:31 -0600

    Update UpdateRebootNotice.ps1

--- UpdateRebootNotice.ps1
+++ UpdateRebootNotice.ps1
@@ -1,22 +1,10 @@
-#Checking if ToastReboot:// protocol handler is present
-New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT -erroraction silentlycontinue | out-null
-$ProtocolHandler = get-item 'HKCR:\ToastReboot' -erroraction 'silentlycontinue'
-if (!$ProtocolHandler) {
-    #create handler for reboot
-    New-item 'HKCR:\ToastReboot' -force
-    Set-ItemProperty 'HKCR:\ToastReboot' -name '(DEFAULT)' -value 'url:ToastReboot' -force
-    Set-ItemProperty 'HKCR:\ToastReboot' -name 'URL Protocol' -value '' -force
-    New-ItemProperty -path 'HKCR:\ToastReboot' -propertytype dword -name 'EditFlags' -value 2162688
-    New-item 'HKCR:\ToastReboot\Shell\Open\command' -force
-    Set-ItemProperty 'HKCR:\ToastReboot\Shell\Open\command' -name '(DEFAULT)' -value 'C:\Windows\System32\shutdown.exe -r -t 00' -force
-}
-
 Install-Module -Name BurntToast -Force
+Invoke-WebRequest -Uri
 
 $Text1 = New-BTText -Content  "Message from R. K. Black IT";
 $Text2 = New-BTText -Content "Your IT provider has installed updates on your computer at $(get-date). Please select if you'd like to reboot now, or snooze this message."
 $Button = New-BTButton -Content "Snooze" -snooze -id 'SnoozeTime'
-$Button2 = New-BTButton -Content "Reboot now" -Arguments "C:\Windows\System32\shutdown.exe -r -t 00" -ActivationType Protocol
+$Button2 = New-BTButton -Content "Reboot now" -Arguments "C:\Windows\Temp\reboot.cmd"
 $5Min = New-BTSelectionBoxItem -Id 5 -Content '5 minutes'
 $10Min = New-BTSelectionBoxItem -Id 10 -Content '10 minutes'
 $1Hour = New-BTSelectionBoxItem -Id 60 -Content '1 hour'
