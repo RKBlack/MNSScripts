1a4b10240525d7155c2df26338de3d2f6cc66c9d
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2022-02-26 14:17:01 -0600

    Update Start-AdSync.cmd

--- Start-AdSync.cmd
+++ Start-AdSync.cmd
@@ -5,6 +5,27 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+#Sleep Timer
+function Start-Sleep {
+    Param (
+        [Parameter(Mandatory)]
+        $Activity,
+        [Parameter(Mandatory)]
+        $Status,
+        [Parameter(Mandatory)]
+        $Seconds
+    )
+    $doneDT = (Get-Date).AddSeconds($seconds)
+    while($doneDT -gt (Get-Date)) {
+        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
+        $percent = ($seconds - $secondsLeft) / $seconds * 100
+        Write-Progress -Activity $Activity -Status $Status -SecondsRemaining $secondsLeft -PercentComplete $percent
+        [System.Threading.Thread]::Sleep(500)
+    }
+    Write-Progress -Activity $Activity -Status $Status -SecondsRemaining 0 -Completed
+}
+
+#Pop-up Form for Policy
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Select a Policy'
 $form.Size = New-Object System.Drawing.Size(300,200)
@@ -41,19 +62,24 @@ $listBox.Height = 80
 [void] $listBox.Items.Add('Initial')
 
 $form.Controls.Add($listBox)
-
 $form.Topmost = $true
-
 $result = $form.ShowDialog()
 
 if ($result -eq [System.Windows.Forms.DialogResult]::OK)
 {
-    $x = $listBox.SelectedItem
-    Start-AdSyncSyncCycle -PolicyType $x
+    $policy = $listBox.SelectedItem
+    if ($policy -eq "Initial") {
+        $waittime = 180
+    }
+    else {
+        $waittime = 45
+    }
+    Start-AdSyncSyncCycle -PolicyType $policy
+    Start-Sleep -Seconds $waittime -Activity "Syncing" -Status "Running $policy Sync..."
 }
 
-Start-Sleep -Seconds 5
+$starttime = ((Get-Date).AddHours(6).AddSeconds(-$waittime)).AddSeconds(-10)
+Get-ADSyncRunProfileResult | Where-Object {$_.StartDate -gt $starttime} | Format-Table StartDate,ConnectorName,RunProfileName,IsRunComplete,Result
 
 ;:sCode 
-;echo done
 ;pause & goto :eof
