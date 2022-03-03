0b87356839dafde67d690a73332bd2995c62dc48
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2022-03-03 14:43:34 -0600

    Create Remove-MailboxItems.ps1

---
+++ Remove-MailboxItems.ps1
@@ -0,0 +1,55 @@
+function Remove-MailboxItemsFunction () {
+
+    #Check for EXO module and install if needed
+    try {
+        Get-Command | Connect-ExchangeOnline
+    }
+    catch {
+        Set-ExecutionPolicy RemoteSigned -Force
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+        Install-Module PowershellGet -Force
+        Install-Module -Name ExchangeOnlineManagement -Force
+    }
+
+    #Connect to EXO - Will prompt user for Modern Auth
+    Connect-ExchangeOnline
+
+
+
+    param (
+        [Parameter(Mandatory)]
+        [string[]]$StartDate,
+        [Parameter(Mandatory)]
+        [string[]]$EndDate,
+        [Parameter(Mandatory)]
+        [string[]]$UserPrincipalName,
+        [Parameter(Mandatory)]
+        [int[]]$DayRange
+    )
+
+    #Format Dates
+    $fstartdate = Get-Date "$startdate"
+    $fenddate = Get-Date "$enddate"
+    $fnextstartdate = $fstartdate
+    $fnextenddate = $fnextstartdate.AddDays($DayRange)
+    $fremainderdays = (New-TimeSpan -Start $fstartdate -End $fenddate).Days % $DayRange - 1
+    
+
+    #Run delete for each block of dates
+    Write-Host "Removing mail items for $UserPrincipalName from $StartDate to $EndDate in blocks of $DayRange days."
+    Read-Host "Press ENTER to continue..." 
+    while ($fnextenddate -lt $fenddate) {
+        $gnextstartdate = Get-Date $fnextstartdate -Format MM/dd/yyyy
+        $gnextenddate = Get-Date $fnextenddate -Format MM/dd/yyyy
+        Write-Host "Deleting from $gnextstartdate to $gnextenddate"
+        Search-Mailbox -Identity $UserPrincipalName -SearchQuery {received:$gnextstartdate..$gnextenddate} -DeleteContent -Force
+        $fnextstartdate = $fnextenddate.AddDays(1)
+        $fnextenddate = $fnextenddate.AddDays($DayRange)
+    }
+    $gnextstartdate = Get-Date $fnextstartdate -Format MM/dd/yyyy
+    $gnextenddate = Get-Date $fnextenddate -Format MM/dd/yyyy
+    $flastenddate = $fnextstartdate.AddDays($fremainderdays)
+    $glastenddate = Get-Date $flastenddate -Format MM/dd/yyyy
+    Write-Host "Deleting from $gnextstartdate to $glastenddate"
+    Search-Mailbox -Identity $UserPrincipalName -SearchQuery {received:$gnextstartdate..$gnextenddate} -DeleteContent -Force
+}
